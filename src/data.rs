960913de599bc0a1c272d5dc427c5faff26d08d0
supersim321 <sss.simar123@gmail.com>
2025-04-01 16:02:27 -0400

    changed products, prices and descriptions

--- src/data.rs
+++ src/data.rs
@@ -5,73 +5,73 @@ pub fn fetch_products(_settings: &Settings) -> Vec<Product> {
     vec![
         Product {
             id: 1,
-            name: "Contoso Catnip's Friend".to_string(),
-            price: 9.99,
-            description: "Watch your feline friend embark on a fishing adventure with Contoso Catnip's Friend toy. Packed with irresistible catnip and dangling fish lure.".to_string(),
-            image: "/catnip.jpg".to_string()
+            name: "Beats By Dr. Dre Studio Pro Over-Ear Noise Cancelling Bluetooth Headphones".to_string(),
+            price: 469.99,
+            description: "Beats By Dr. Dre Studio Pro over-ear headphones with Active Noise Cancelling blocks unwanted external noise, while Transparency Mode lets outside sound in".to_string(),
+            image: "/beats.jpg".to_string()
         },
         Product {
             id: 2,
-            name: "Salty Sailor's Squeaky Squid".to_string(),
-            price: 6.99,
-            description: "Let your dog set sail with the Salty Sailor's Squeaky Squid. This interactive toy provides hours of fun, featuring multiple squeakers and crinkle tentacles.".to_string(),
-            image: "/squid.jpg".to_string()
+            name: "ASUS ROG Swift 26.5' QHD 240Hz 0.03ms GTG OLED G-Sync Gaming Monitor".to_string(),
+            price: 1099.99,
+            description: "Bring your games to life with the ASUS ROG Swift 26.5' gaming monitor. It features a 26.5' OLED display with QHD resolution and 240Hz refresh rate that keep your gaming action detailed and crisp".to_string(),
+            image: "/asus.jpg".to_string()
         },
         Product {
             id: 3,
-            name: "Mermaid's Mice Trio".to_string(),
-            price: 12.99,
-            description: "Entertain your kitty with the Mermaid's Mice Trio. These adorable plush mice are dressed as mermaids and filled with catnip to captivate their curiosity.".to_string(),
-            image: "/mermaid.jpg".to_string()
+            name: "Blue Microphones Yeti USB Microphone - Silver".to_string(),
+            price: 129.99,
+            description: "Transform the audio of your recordings with the Yeti USB microphone. It produces a clear, broadcast-quality sound for music, podcasts, and more.".to_string(),
+            image: "/blueyeti.jpg".to_string()
         },
         Product {
             id: 4,
-            name: "Ocean Explorer's Puzzle Ball".to_string(),
-            price: 11.99,
-            description: "Challenge your pet's problem-solving skills with the Ocean Explorer's Puzzle Ball. This interactive toy features hidden compartments and treats, providing mental stimulation and entertainment.".to_string(),
-            image: "/ocean.jpg".to_string()
+            name: "Apple iPhone 13 128GB - Midnight".to_string(),
+            price: 699.99,
+            description: "Stand out and show-off your superpowers with the iPhone 13. It features the powerful A15 Bionic chip, superfast 5G to download and stream high-quality video, a bright 6.1' Super Retina XDR display, and Ceramic Shield for better drop performance.".to_string(),
+            image: "/iphone13.jpg".to_string()
         },
         Product {
             id: 5,
-            name: "Pirate Parrot Teaser Wand".to_string(),
-            price: 8.99,
-            description: "Engage your cat in a playful pursuit with the Pirate Parrot Teaser Wand. The colorful feathers and jingling bells mimic the mischievous charm of a pirate's parrot.".to_string(),
-            image: "/pirate.jpg".to_string()
+            name: "Apple - 10.9-Inch iPad (10th Generation)".to_string(),
+            price: 329.99,
+            description: "A tablet with 64GB storage and Wi-Fi connectivity, suitable for multimedia, productivity, and creative tasks.".to_string(),
+            image: "/ipad.jpg".to_string()
         },
         Product {
             id: 6,
-            name: "Seafarer's Tug Rope".to_string(),
-            price: 14.99,
-            description: "Tug-of-war meets nautical adventure with the Seafarer's Tug Rope. Made from marine-grade rope, it's perfect for interactive play and promoting dental health in dogs.".to_string(),
-            image: "/tug.jpg".to_string()
+            name: "Apple - 13-inch MacBook Air with M2 Chip".to_string(),
+            price: 999.99,
+            description: "Features a new design, 16GB memory, 256GB SSD, and the Apple M2 chip for enhanced performance. Perfect for productivity and portability.".to_string(),
+            image: "/macbook.jpg".to_string()
         },
         Product {
             id: 7,
-            name: "Seashell Snuggle Bed".to_string(),
-            price: 19.99,
-            description: "Give your furry friend a cozy spot to curl up with the Seashell Snuggle Bed. Shaped like a seashell, this plush bed provides comfort and relaxation for cats and small dogs.".to_string(),
-            image: "/bed.jpg".to_string()
+            name: "Logitech G502 LIGHTSPEED 25600 DPI Wireless Optical Gaming Mouse - Black".to_string(),
+            price: 149.99,
+            description: "Play better with the Logitech G502 LIGHTSPEED gaming mouse. It brings you superfast LIGHTSPEED wireless, a 1ms response, and POWERPLAY compatibility. Eleven programmable buttons, 16.8 million colours, and 6 adjustable, configurable weights make the mouse uniquely yours.".to_string(),
+            image: "/logitech.jpg".to_string()
         },
         Product {
             id: 8,
-            name: "Nautical Knot Ball".to_string(),
-            price: 7.99,
-            description: "Unleash your dog's inner sailor with the Nautical Knot Ball. Made from sturdy ropes, it's perfect for fetching, tugging, and satisfying their chewing needs.".to_string(),
-            image: "/knot.jpg".to_string()
+            name: "Razer Huntsman Mini Backlit Optical Gaming Keyboard - White".to_string(),
+            price: 109.99,
+            description: "Speed is crucial when it comes to gameplay and this Razer Huntsman Mini backlit optical gaming keyboard provides fast, light, and smooth actuations to enhance the way you play. ".to_string(),
+            image: "/razerhuntsman.jpg".to_string()
         },
         Product {
             id: 9,
-            name: "Contoso Claw's Crabby Cat Toy".to_string(),
-            price: 3.99,
-            description: "Watch your cat go crazy for Contoso Claw's Crabby Cat Toy. This crinkly and catnip-filled toy will awaken their hunting instincts and provide endless entertainment.".to_string(),
-            image: "/crabby.jpg".to_string()
+            name: "Samsung 85' 4K UHD HDR QLED Tizen OS Smart TV".to_string(),
+            price: 2399.99,
+            description: "Enjoy the next level of visual immersion with this Samsung Smart TV. Featuring 4K Ultra HD resolution and 4K AI Upscaling, you can expect impressive picture quality every time.".to_string(),
+            image: "/samsungtv.jpg".to_string()
         },
         Product {
             id: 10,
-            name: "Ahoy Doggy Life Jacket".to_string(),
-            price: 5.99,
-            description: "Ensure your furry friend stays safe during water adventures with the Ahoy Doggy Life Jacket. Designed for dogs, this flotation device offers buoyancy and visibility in style.".to_string(),
-            image: "/lifejacket.jpg".to_string()
+            name: "Xbox Series S 512GB Console".to_string(),
+            price: 379.99,
+            description: "Combining speed and performance, the Xbox Series S console lets you play games your way.".to_string(),
+            image: "/xbox.jpg".to_string()
         }
     ]
 }
